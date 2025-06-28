960ec621385ca07f39a4a0d95d2dba996d030e42
Saravanan K S <saravananks@presidio.com>
2025-06-28 12:35:32 +0530

    remove ununsed

--- vite-teams-audio-bot/main.js
+++ vite-teams-audio-bot/main.js
@@ -5,10 +5,13 @@ let callAgent;
 let activeCall;
 let audioContext = null;
 
-const token = "";
-const meetingLink = "";
-const speechKey = "";
-const region = ""; // e.g., "eastus"
+const token =
+  "eyJhbGciOiJSUzI1NiIsImtpZCI6IkRCQTFENTczNEY1MzM4QkRENjRGNjA4NjE2QTQ5NzFCOTEwNjU5QjAiLCJ4NXQiOiIyNkhWYzA5VE9MM1dUMkNHRnFTWEc1RUdXYkEiLCJ0eXAiOiJKV1QifQ.eyJza3lwZWlkIjoiYWNzOjBjM2U1OGJlLTRkNGQtNDM5Yy1iOGY4LTYyNDcwNzE3N2UzN18wMDAwMDAyOC01MTZmLTQ1YjYtN2RmNy0zYTNhMGQwMDUwOWUiLCJzY3AiOjE3OTIsImNzaSI6IjE3NTEwMzA1MzgiLCJleHAiOjE3NTExMTY5MzgsInJnbiI6ImFtZXIiLCJhY3NTY29wZSI6ImNoYXQsdm9pcCIsInJlc291cmNlSWQiOiIwYzNlNThiZS00ZDRkLTQzOWMtYjhmOC02MjQ3MDcxNzdlMzciLCJyZXNvdXJjZUxvY2F0aW9uIjoidW5pdGVkc3RhdGVzIiwiaWF0IjoxNzUxMDMwNTM4fQ.PKGnxJjjHbizZvGvwL99NlUPE7Nc3SCSpS4sgvO0B7g6Ix9p1iPGILE4VmeNNy8iENW3ST8tUqWPU_U05k3loanw_KgV9OzspxN6ml0o_1z8bW5lNQEUuh-j2C5Hd2z-Zn310fMloYEJfKU2s8KU45t5VHPg_Ktp4kne69cgTFizEByu6bDC61xL2FLfMZEq7r9d82nZgV9vw9xsxLI_feq7ruaX6ppy70wfxcUBtjoqUWL8by2_3mDUqPagz5BcO_pZEqxZqtcW8tDN3bAvzEuoVsNyCFxAwfPzgsqpwdp3vQvvXPLj3LQolWjYSu1IqKpKsy2sTI_qBqYaAosxdg";
+const meetingLink =
+  "https://teams.microsoft.com/l/meetup-join/19%3ameeting_YmE1OWJlODAtMWY5OS00Y2Q5LWFlNjAtOTc1MmU0Y2I5NTU4%40thread.v2/0?context=%7b%22Tid%22%3a%22f6002a3e-5bf9-42fd-85ba-f5e499d8efb0%22%2c%22Oid%22%3a%22af8c340f-bc9e-4ad1-a90a-c751bf29d898%22%7d";
+const speechKey =
+  "Aaq8N0KEKKRSxm5bTm1fWXY7HfOsfObwehBmlIKQYxWxSSbsWqZvJQQJ99BFACYeBjFXJ3w3AAAYACOG4SsS";
+const region = "eastus"; // e.g., "eastus"
 
 export async function joinAndPlay() {
   const callClient = new CallClient();
